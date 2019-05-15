015d07261ee535a8e93865a2c8d46b5792eee719
Gaston Avila <avila.gas@gmail.com>
2019-05-15 17:17:34 -0300

    Render de una lista

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -5,6 +5,10 @@ file = open('cgi-bin/index.html')
 template = file.read()
 file.close()
 
+body = ['Gaston', 'Verónica', 'Jerónimo']
+
+html = template.format(body=body)
+
 print("Content-Type: text/html\n")
 print()
-print(template)
+print(html)
