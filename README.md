fa540c1d6628b1b415f541b7f3c843694718d1a4
Gaston Avila <avila.gas@gmail.com>
2019-05-15 17:14:14 -0300

    Leer un template y renderizarlo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Introduccion a la web, base de datos e inyeccion sql
+
+Como ejecutar en python 3
+```
+python -m http.server --cgi 8000
+```
--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python3
 
+# Generar la salida
+file = open('cgi-bin/index.html')
+template = file.read()
+file.close()
+
 print("Content-Type: text/html\n")
 print()
-print("Hola mundo")
+print(template)
